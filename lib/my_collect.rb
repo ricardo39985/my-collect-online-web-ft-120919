6535c9b0ac9b47f5f4ecd56fd67f963e95f9423e
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-12 23:29:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(array)
   new_array = []
   if block_given?
-    new_array << .each { |e|  yield (e)}
+    new_array.push( array.each { |e|  yield (e)})
     new_array
   else
     "No block given"
