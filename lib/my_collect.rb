13001b9abe99757cc4d371060568d61f0d3c8902
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-12 23:38:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(array)
+  new_array = []
+  if block_given?
+    i = 0
+    while i < array.length
+      new_array.push(yield array[i])
+      i+=1
+    end
+    return new_array
+  else
+    "No block given"
+  end
+end
